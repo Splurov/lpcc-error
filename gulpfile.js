215e6ee2e1460a82dfee8c3324a40bfa733c8900
Mikhail Kalashnik <m.kalashnik@hh.ru>
2015-06-04 14:52:00 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var gulp = require('gulp');
+var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
+var LessPluginCleanCSS = require('less-plugin-clean-css');
+
+var cleancss = new LessPluginCleanCSS();
+
+gulp.task('css', function() {
+    return gulp.src('example.less')
+        .pipe(sourcemaps.init())
+        .pipe(less({
+                  plugins: [cleancss]
+              }))
+        .pipe(sourcemaps.write('.', {
+                  sourceRoot: 'build/'
+              }))
+        .pipe(gulp.dest('build/'));
+});
