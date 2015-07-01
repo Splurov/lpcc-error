3b13cd3e0b33e76593c97b91655d7064cee7ec78
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2015-07-01 09:29:47 +0100

    Solves https://github.com/jakubpawlowicz/clean-css/issues/593

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
 var LessPluginCleanCSS = require('less-plugin-clean-css');
 
-var cleancss = new LessPluginCleanCSS();
+var cleancss = new LessPluginCleanCSS({ sourceMapInlineSources: true });
 
 gulp.task('css', function() {
     return gulp.src('example.less')
